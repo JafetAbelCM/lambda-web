0e429bef3cedfad01ca71c2c1054ff03cc2f3c93
Jafet Carrasco <jafet.carrasco@tecsup.edu.pe>
2021-12-10 12:14:17 -0500

    Add Me/Index page

---
+++ resources/js/Pages/Me/Index.vue
@@ -0,0 +1,25 @@
+<template>
+    <AppLayout title="Me">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ $page.props.user.name }}
+            </h2>
+        </template>
+
+    </AppLayout>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import AppLayout from "../../Layouts/AppLayout";
+
+export default defineComponent({
+    components: {
+        AppLayout
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
